ac8cd49d9961948a972eb90008b48e214e89b705
Ykk2001 <yogeshkhatake18@gmail.com>
2025-12-07 16:21:53 +0530

    Done2

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
-
 import React from "react";
-import { BrowserRouter,Switch ,Route} from 'react-router-dom'
-import './../styles/App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./../styles/App.css";
 import Layout from "./Layout";
 import Women from "./Women";
 const App = () => {
   return (
     <BrowserRouter>
-        {/* Do not remove the main div */}
-  
+      {/* Do not remove the main div */}
+      <main>
+       <div>
         <Switch>
-          <Route exact path='/' component={Layout}/>
-          <Route path='/women' component={Women}/>    
+          <Route exact path="/" component={Layout} />
+          <Route path="/women" component={Women} />
         </Switch>
+      </div>
+      </main>
+      
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
